f00dc8b52b50c44e20e968312bd92a7cd11c8d6d
Niklaus Maurer <nick.is.normal@gmail.com>
2021-11-27 15:51:05 +0100

    adds threading

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
+import subprocess
+import threading
+
 from flask import Flask, request
 import json
 
 app = Flask(__name__)
 
 
+def stream_video():
+    subprocess.run(["/home/nmaurer/code/NiklausMaurer/chromecast-player/test-executable.sh",
+                    "/home/nmaurer/code/NiklausMaurer/chromecast-player/output.txt"])
+
+
 @app.route('/', methods=['PUT'])
 def set_play_state():  # put application's code here
     data = json.loads(request.data)
+
+    thread = threading.Thread(target=stream_video)
+    thread.start()
+
     return data['url']
 
 
